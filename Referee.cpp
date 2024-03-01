5bfd56066e7a03a7eeb5ca9ea6c29054a84464d1
Moosh56 <mischa@COM7046>
2024-03-01 10:36:31 +1030

    This is stupid

--- Referee.cpp
+++ Referee.cpp
@@ -14,14 +14,14 @@ Player* Referee::refGame(Player* player1, Player* player2){
     char p2 = player2->makeMove();
 
     if(p1 == p2){
-        cout << "It's a Tie" << endl;
+        // cout << "It's a Tie" << endl;
         return nullptr;
     }
     if(p2 == interactions[p1]){
-        cout << player2->getName() << " Wins" << endl;
+        // cout << player2->getName() << " Wins" << endl;
         return player2;
     } else {
-        cout << player1->getName() << " Wins" << endl;
+        // cout << player1->getName() << " Wins" << endl;
         return player2;
     }
 }
