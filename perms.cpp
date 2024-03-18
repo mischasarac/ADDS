bbefd58b99b04a6214a611250625f8ffad1c02f6
Moosh56 <mischa@COM7046>
2024-03-18 11:46:50 +1030

    kms

---
+++ perms.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+void perms(string letters, int size){
+    for(int i = 0; i < letters.size(); i++){
+        perms.erase(perms.begin()+i)
+    }
+}
+
+int main(){
+
+}
