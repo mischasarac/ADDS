25e27b3c458d531d42c6c6c9dc553f6594bc115d
Moosh56 <mischa@COM7046>
2024-04-26 10:15:44 +0930

    This is shit

--- BigNumCalc.h
+++ BigNumCalc.h
@@ -13,9 +13,9 @@ class BigNumCalc {
 public:
     list<int> buildBigNum(string numString);
 
-    std::list<int> add(std::list<int> num1, std::list<int> num2);
-    std::list<int> sub(std::list<int> num1, std::list<int> num2);
-    std::list<int> mul(std::list<int> num1, std::list<int> num2);
+    list<int> add(list<int> num1, list<int> num2);
+    list<int> sub(list<int> num1, list<int> num2);
+    list<int> mul(list<int> num1, list<int> num2);
 
 };
 
